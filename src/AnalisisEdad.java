a15d404415291b616773647368759fecaebac95d
Gonzalo <gonzalo.mrdavila@hotmail.com>
2024-05-28 10:49:01 +0200

    Subida de cambios

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,9 +1,45 @@
+	import java.util.ArrayList;
 
-public class AnalisisEdad {
+	public class AnalisisEdad {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList <Integer> x = new ArrayList<Integer>();
 
+		public AnalisisEdad(ArrayList<Integer> x) {
+			this.x = x;
+		}
+
+		public ArrayList<Integer> getX() {
+			return x;
+		}
+
+		public void setX(ArrayList<Integer> x) {
+			this.x = x;
+		}
+
+		public String comprobarArray(ArrayList<Integer> x) {
+			int menores = 0, mayores = 0, jubilados = 0;
+			String res;
+			for(int i = 0; i < x.size(); i++) {
+				if(i < 18) {
+					menores ++;
+				}
+				else if(i > 18) {
+					mayores ++;
+				}
+				else if(i > 65) {
+					jubilados ++;
+				}
+			}
+			res = "Menores de Edad: " + menores + ", Mayores de Edad: " + mayores + ", Jubilados: " + jubilados;
+			return res;
+		}
+		
+		@Override
+		public String toString() {
+			return "AnalisisEdad [x=" + x + "]";
+		}
+		
 	}
+
 
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class PrincipalApp {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> edades = new ArrayList<Integer>();
+		String resultado;
+		int x;
+		for(int i = 0; i < 10; i++) {
+			x = (int)(Math.random()*100);
+			edades.add(x);
+		}
+		
+		AnalisisEdad a = new AnalisisEdad(edades);
+		
+		resultado = a.comprobarArray(edades);
+		
+		System.out.println(resultado);
+
+	}
 
 }
